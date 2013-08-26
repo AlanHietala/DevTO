05ea632db70de71a744113f8101329ff763e2400
Alan Hietala <alan.hietala@gmail.com>
2013-08-25 23:41:58 -0400

    updating solution files

--- scripts/controllers/listctrl.js
+++ scripts/controllers/listctrl.js
@@ -1,10 +1,22 @@
 var ListCtrl = function ($scope) {
+
     // 1. add attendeeList model to the scope
     //    array should contain models in the format:
     //    {name: 'foo', present: false}
+    $scope.attendeeList = [
+        {name: 'Alan Hietala', present: false},
+        {name: 'Mary Smith', present: false}
+    ];
 
     // 2. add addPerson function to the scope that will push
     //    a new person object onto the attendeeList array
     //    get the name value from the $scope.newPerson variable
     //    as declared in the HTML Template ng-model directive
+    $scope.addPerson = function () {
+        if ($scope.newPerson !== '') {
+            $scope.attendeeList.push({name: $scope.newPerson, present: false});
+            $scope.newPerson = '';
+        }
+    }
+
 }
