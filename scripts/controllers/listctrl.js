e2c44d489f35f800c11e21994cf06a93cad012be
Alan Hietala <alan.hietala@gmail.com>
2013-08-25 23:21:21 -0400

    Initial Commit

---
+++ scripts/controllers/listctrl.js
@@ -0,0 +1,10 @@
+var ListCtrl = function ($scope) {
+    // 1. add attendeeList model to the scope
+    //    array should contain models in the format:
+    //    {name: 'foo', present: false}
+
+    // 2. add addPerson function to the scope that will push
+    //    a new person object onto the attendeeList array
+    //    get the name value from the $scope.newPerson variable
+    //    as declared in the HTML Template ng-model directive
+}
